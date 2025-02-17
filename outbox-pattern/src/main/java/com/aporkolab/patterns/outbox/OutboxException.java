249d65703ace2019c52b2070d0e3962c037546cc
APorkolab <adam.porkolab@gmail.com>
2025-02-17 11:45:22 +0100

    feat(outbox): add OutboxException

---
+++ outbox-pattern/src/main/java/com/aporkolab/patterns/outbox/OutboxException.java
@@ -0,0 +1,12 @@
+package com.aporkolab.patterns.outbox;
+
+public class OutboxException extends RuntimeException {
+
+    public OutboxException(String message) {
+        super(message);
+    }
+
+    public OutboxException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
